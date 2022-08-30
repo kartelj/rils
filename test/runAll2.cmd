208f6db532daa1f5d2852185ac9fe3212e15b6a5
Aleksandar <aleksandar.kartelj@gmail.com>
2022-08-31 00:30:13 +0200

    Returned to previous multi-file organization.

--- test/runAll2.cmd
+++ test/runAll2.cmd
@@ -1,3 +1,5 @@
-runMultiple.cmd 120 1000000 0.001 0.1
-
+runMultiple.cmd 3600 1000000 0.001 0
+runMultiple.cmd 3600 1000000 0.001 0.001
+runMultiple.cmd 3600 1000000 0.001 0.01
+runMultiple.cmd 3600 1000000 0.001 0.1
 
