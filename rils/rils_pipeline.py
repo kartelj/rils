483120b90f26037f81b0ec32bd3a260380c8ca1d
Aleksandar <aleksandar.kartelj@gmail.com>
2023-08-10 22:33:11 +0200

    RILSPipelineRegressor added.

---
+++ rils/rils_pipeline.py
@@ -0,0 +1,48 @@
+
+import math
+from sklearn.base import BaseEstimator
+from sympy import preorder_traversal
+
+from rils.rils_ensemble import RILSEnsembleRegressor
+from rils.solution import Solution
+
+
+class RILSPipelineRegressor(BaseEstimator):
+
+    def __init__(self, epochs=100, fit_calls_per_epoch=100000, max_seconds_per_epoch=10000, initial_sample_size=1,parallelism = 10, init_target_size=20,verbose=False,  random_state=0):
+        self.inner_ensemble = RILSEnsembleRegressor(epochs=epochs, fit_calls_per_epoch=fit_calls_per_epoch, max_seconds_per_epoch=max_seconds_per_epoch, initial_sample_size=initial_sample_size, parallelism=parallelism, target_size=init_target_size, verbose=verbose, random_state=random_state)
+        
+    def fit(self, X, y, init_sympy_sol_str = "0", dataset_file="", X_test = None, y_test = None):
+        self.inner_ensemble.fit(X, y ,init_sympy_sol_str=init_sympy_sol_str, dataset_file=dataset_file, X_test=X_test, y_test=y_test)
+        best_tradeoff_sympy_sol = self.select_best_tradeoff_solution()
+        print("The best tradeoff model is "+best_tradeoff_sympy_sol)
+        self.model_simp = best_tradeoff_sympy_sol
+        self.model = Solution([Solution.convert_to_my_nodes(self.model_simp)])
+        return (self.model, self.model_simp)
+
+    def select_best_tradeoff_solution(self):
+        # reading backwards and finding the last relevant improvement w.r.t. R2
+        # relevant improvement is one of at least 0.5% improvement in R2
+        r2_with_sols = self.inner_ensemble.best_solutions
+        i = len(r2_with_sols)-1
+        while i>0:
+            if r2_with_sols[i][0]-r2_with_sols[i-1][0]>0.005:
+                break
+            i-=1
+        return r2_with_sols[i][1]
+    
+    def size(self):
+        if self.model_simp is not None:
+            return self.complexity(self.model_simp)
+        return math.inf
+    
+    def complexity(self, sm):
+        c=0
+        for arg in preorder_traversal(sm):
+            c += 1
+        return c
+
+    def model_string(self):
+        if self.model_simp is not None:
+            return str(self.model_simp)
+        return ""
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='rils',
+    version='0.3',
+    description='Regression via Iterated Local Search for Symbolic Regression GECCO Competition -- 2023',
+    long_description= long_description,
+    long_description_content_type  = "text/markdown",
+    author='Aleksandar Kartelj, Marko Đukanović',
+    author_email='aleksandar.kartelj@gmail.com',
+    url='https://github.com/kartelj/rils',
+    packages = find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License", 
+        "Operating System :: OS Independent"
+    ],
+    python_requires= ">=3.6",
+    py_modules=["rils"],
+    package_dir = {'rils':'rils'}, 
+    install_requires=["numpy", "sympy", "scikit-learn","statsmodels"],
+)
